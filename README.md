474c26f331286747dda3a522f2e126cb7da5055a
xfhy <xfhy666@gmail.com>
2017-10-03 19:19:36 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # EasyBanner
-一个轻松实现简单banner的框架，轻松实现。。。。
+
+> 一个轻松实现简单banner的框架，轻松实现。。。。
+
+### 先上效果图：
+![](http://olg7c0d2n.bkt.clouddn.com/17-10-3/64062219.jpg)]
+
+### 已实现的功能
+* 自动轮播
+* 无限左划右划
+* 加载网络图片（Glide），加载文字
+* 底部小白点可切换，大小可换，数量可动态增加
+* 触摸时不能滑动
+* 实现banner的item点击事件
+* 广告数目可以随意
+* 可以在代码中生成banner，也可以在xml布局中生成
+
+## 简单使用
+
+> 使用前将该EasyBanner 这个module导入到自己的项目，并在app主项目中依赖此module.
+
+### 1.在xml中使用
+
+``` xml
+<com.xfhy.easybanner.ui.EasyBanner
+    android:id="@+id/eb_banner"
+    android:layout_width="match_parent"
+    android:layout_height="200dp"/>
+```
+
+``` java
+//可以在布局里面写
+mBanner = (EasyBanner) findViewById(R.id.eb_banner);
+mBanner.initBanner(getImageUrlData(), getContentData());
+```
+
+### 2.在代码中使用banner
+
+``` java
+//也可以直接动态生成
+EasyBanner easyBanner = new EasyBanner(this);
+easyBanner.initBanner(getImageUrlData(), getContentData());
+mRootView.addView(easyBanner,new LinearLayout.LayoutParams(LinearLayout
+        .LayoutParams.MATCH_PARENT, DensityUtil.dip2px(this,200)));
+```
+
+### 3.实现点击事件
+
+```java
+//监听banner的item点击事件
+mBanner.setOnItemClickListener(new EasyBanner.OnItemClickListener() {
+    @Override
+    public void onItemClick(int position, String title) {
+        Toast.makeText(MainActivity.this, "position:"+position+"   title:"+title, Toast
+                .LENGTH_SHORT).show();
+    }
+});
+```
---
+++ app/src/main/java/com/example/xfhy/bannerdemo/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.xfhy.bannerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+
+import com.xfhy.easybanner.ui.EasyBanner;
+import com.xfhy.easybanner.util.DensityUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EasyBanner mBanner;
+    private LinearLayout mRootView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mRootView = (LinearLayout) findViewById(R.id.ll_main);
+
+        //可以在布局里面写
+        mBanner = (EasyBanner) findViewById(R.id.eb_banner);
+        mBanner.initBanner(getImageUrlData(), getContentData());
+        //监听banner的item点击事件
+        mBanner.setOnItemClickListener(new EasyBanner.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, String title) {
+                Toast.makeText(MainActivity.this, "position:"+position+"   title:"+title, Toast
+                        .LENGTH_SHORT).show();
+            }
+        });
+
+        //也可以直接动态生成
+        EasyBanner easyBanner = new EasyBanner(this);
+        easyBanner.initBanner(getImageUrlData(), getContentData());
+        mRootView.addView(easyBanner,new LinearLayout.LayoutParams(LinearLayout
+                .LayoutParams.MATCH_PARENT, DensityUtil.dip2px(this,200)));
+    }
+
+    private List<String> getImageUrlData() {
+        List<String> imageList = new ArrayList<>();
+        imageList.add("https://pic3.zhimg.com/v2-e885c8acf8ca274cda11dd8ce7760d26.jpg");
+        imageList.add("https://pic3.zhimg.com/v2-ecfc08ebca9f42e29144d09050fb619e.jpg");
+        imageList.add("https://pic1.zhimg.com/v2-0d3e0c7d778083003e6f8029cf7cf570.jpg");
+        imageList.add("https://pic1.zhimg.com/v2-5d0f06845b6cdd5e32aed0a960aede98.jpg");
+        imageList.add("https://pic2.zhimg.com/v2-4d873d82642d347aa0e709b2e2f5be81.jpg");
+        return imageList;
+    }
+
+    private List<String> getContentData() {
+        List<String> contentList = new ArrayList<>();
+        contentList.add("黄河变清了要发生大洪水」，这个标题党过分了");
+        contentList.add("地铁司机为什么每次都能把车停得那么准？");
+        contentList.add("赵薇老师，你还能不能有点明星样儿了？");
+        contentList.add("听说同时吃它俩，像是在吃「蘸了肥皂水的苍蝇」我亲自试了试");
+        contentList.add("「不主动追求你，也不明确拒绝你」，这就是现代人的爱情");
+        return contentList;
+    }
+}
